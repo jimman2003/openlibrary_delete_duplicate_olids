5bedf1b0ed1c43ce0f11044b134b28bcd47144de
jimman2003 <jimman2003@hotmail.com>
2021-11-22 20:13:37 +0200

    add verification before editing

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ with open("dup.txt") as f:
             for author in work.authors:
                 if author not in new_authors:
                     new_authors.append(author)
-            work.authors=new_authors
-            #work.save("removed duplicate olid of author")
+            if work.authors==new_authors:
+                print(f"Skip {work.olid}")
+            else:
+                work.authors=new_authors
+                print(f"Deduplicated {work.olid}")
+                work.save("Removed duplicate olid of author")
            
